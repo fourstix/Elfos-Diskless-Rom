8aa1cf010408348a3412a6a484a8b10bb20a2a81
fourstix <fourstix@gmail.com>
2023-11-05 15:11:42 -0500

    Removed user path information from build file

--- pico/build/pico_build.bat
+++ pico/build/pico_build.bat
@@ -1,8 +1,8 @@
-C:\Users\fours\Utilities\Asm02\asm02.exe -i -L -s -DPICOELF bios.asm
-C:\Users\fours\Utilities\Asm02\asm02.exe -i -L -s -DPICOROM diskless.asm
-C:\Users\fours\Utilities\Asm02\asm02.exe -i -L -s -DPICOROM edtasm.asm
-C:\Users\fours\Utilities\Asm02\asm02.exe -i -L -s -DPICOROM visual02.asm
-C:\Users\fours\Utilities\Asm02\asm02.exe -i -L -s -DPICOROM forth.asm
-C:\Users\fours\Utilities\Asm02\asm02.exe -i -L -s -DPICOROM rclisp.asm
-C:\Users\fours\Utilities\Asm02\asm02.exe -i -L -s -DPICOROM rcbasic.asm
-C:\Users\fours\Utilities\Asm02\asm02.exe -i -L -s -DPICOROM vtl2.asm
+[Your_Path]\Asm02\asm02.exe -i -L -s -DPICOELF bios.asm
+[Your_Path]\Asm02\asm02.exe -i -L -s -DPICOROM diskless.asm
+[Your_Path]\Asm02\asm02.exe -i -L -s -DPICOROM edtasm.asm
+[Your_Path]\Asm02\asm02.exe -i -L -s -DPICOROM visual02.asm
+[Your_Path]\Asm02\asm02.exe -i -L -s -DPICOROM forth.asm
+[Your_Path]\Asm02\asm02.exe -i -L -s -DPICOROM rclisp.asm
+[Your_Path]\Asm02\asm02.exe -i -L -s -DPICOROM rcbasic.asm
+[Your_Path]\Asm02\asm02.exe -i -L -s -DPICOROM vtl2.asm
